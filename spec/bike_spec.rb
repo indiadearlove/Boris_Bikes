080bdd783de84fb15898cae9618654bc332296e9
India Dearlove <india_dearlove@yahoo.co.uk>
2014-12-10 12:08:25 +0000

    fixed indentation2

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,16 +1,14 @@
 require 'bike'
 
-describe Bike do
-	
+describe Bike do  
   it 'should not be broken after we create it' do
     bike = Bike.new
-	expect(bike).not_to be_broken
-	end
-
+    expect(bike).not_to be_broken
+  end
 
   it 'should be able to break' do
-	bike = Bike.new
-	bike.break!
-	expect(bike).to be_broken
-end
+    bike = Bike.new
+    bike.break!
+    expect(bike).to be_broken
+  end
 end
