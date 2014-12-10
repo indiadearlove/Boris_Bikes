de0e592837db3cb10679fb4088c8392aa57230d1
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-10 14:35:30 +0000

    added capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,10 @@ require 'docking_station'
 
 describe DockingStation do 
 
+  def fill_station(station)
+    20.times {station.dock(Bike.new)}
+  end
+
   let (:bike) {Bike.new}
   let (:station) {DockingStation.new(:capacity =>20)}
 
@@ -19,8 +23,13 @@ describe DockingStation do
 
   it "should know when it's full" do
     expect(station).not_to be_full
-    20.times {station.dock(Bike.new)}
+    fill_station station
     expect(station).to be_full
+  end
+
+  it "should not accept a bike if it\'s full" do
+    fill_station station
+    expect(lambda { station.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
   end
 
 end
